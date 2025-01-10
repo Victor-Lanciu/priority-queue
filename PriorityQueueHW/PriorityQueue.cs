46d80ed14580cc952fb7a9abc973feac540a8b88
Victor Lanciu <wlanciu@gmail.com>
2025-01-10 11:07:00 +0200

    Added homework

---
+++ PriorityQueueHW/PriorityQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueueHW
+{
+    public class PriorityQueue<T>
+    {
+        private class PriorityQueueItem<T>
+        {
+            public T Element { get; set; }
+            public int Priority { get; set; }
+            public PriorityQueueItem(T element, int priority)
+            {
+                Element = element;
+                Priority = priority;
+            }
+        }
+        private List<PriorityQueueItem<T>> elements = new List<PriorityQueueItem<T>>();
+
+        public void Enqueue(T element, int priority)
+        {
+            var queueItem = new PriorityQueueItem<T>(element, priority);
+            elements.Add(queueItem);
+            Console.WriteLine($"A fost adaugat elementul {element} cu prioritatea {priority}");
+            elements = elements.OrderByDescending(e => e.Priority).ToList();
+        }
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Coada este goala");
+            }
+            var highestPriorityItem = elements[0];
+            elements.RemoveAt(0);
+            return highestPriorityItem.Element;
+        }
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Coada este goala");
+            }
+            return elements[0].Element;
+        }
+
+        public bool IsEmpty()
+        {
+            return elements.Count == 0;
+        }
+
+    }
+}
---
+++ PriorityQueueHW/Program.cs
@@ -0,0 +1,34 @@
+﻿using PriorityQueueHW;
+
+namespace CoadaPrioritate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var priorityQueueInt = new PriorityQueue<int>();
+
+            priorityQueueInt.Enqueue(12, 10);
+            priorityQueueInt.Enqueue(31, 11);
+            priorityQueueInt.Enqueue(444, 4);
+
+
+            while (!priorityQueueInt.IsEmpty())
+            {
+                Console.WriteLine(priorityQueueInt.Dequeue());
+            }
+            var priorityQueueString = new PriorityQueue<string>();
+
+            priorityQueueString.Enqueue("Task 1", 1);
+            priorityQueueString.Enqueue("Task 4", 5);
+            priorityQueueString.Enqueue("Task 2", 3);
+
+            while (!priorityQueueString.IsEmpty())
+            {
+                Console.WriteLine(priorityQueueString.Dequeue());
+            }
+            Console.WriteLine(priorityQueueInt.Peek());
+
+        }
+    }
+}
